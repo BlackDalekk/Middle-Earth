bc4ee31363d3849ee9bec77ff7143ac7bcffe009
Ivan Bakaev <bakaev_ivan@rambler.ru>
2021-05-15 17:07:59 +0300

    добавлена карта, песок, вода, движение камеры. почищен код

--- main.cpp
+++ main.cpp
@@ -4,14 +4,6 @@
 #include <time.h>
 #include <thread>
 #include "objs.h"
-//
-////можно реализовать наследованени
-////класс graund -> камен или трава и инициализировать их на моменте создания.
-////таким образом снижаем общую потреб память так как испоьзуем всего M x N объектов , вместо K x M x N объектов
-////проблема в доступе к члену sprite в стоках типа obj[i]->sprite
-//
-////enum tyLocality {GRASS , ROCK};
-//
 //struct Cell
 //{
 //    int x, y; //левый верхний угол . центр
@@ -24,127 +16,6 @@
 ////21 22 23 ...
 ////31 32 33 ...
 ////  ....
-//
-//struct Rock
-//{
-//    tyLocality ty = ROCK;
-//    //int x1, y1, x2, y2; //верхний левый, нижний левый, верхний правый, нижний левый
-//    int hSp = 60; //высота внутреннего спрайта
-//    int wSp = 80; //ширина внутреннего спрайта
-//    sf::Vector2i origin;
-//    sf::Texture texture;
-//    sf::Sprite sprite;
-//    //Rock() { if (!texture.loadFromFile("rock.png")) cout << "Ошибка загрузки текстуры камня\n"; sprite.setTexture(texture);}
-//};
-//struct Grass
-//{
-//    tyLocality ty = GRASS;
-//    sf::Texture texture;
-//    sf::Sprite sprite;
-//
-//    //Grass() { if (!texture.loadFromFile("grass.png")) cout << "Ошибка загрузки текстуры травы\n"; sprite.setTexture(texture); }
-//};
-//struct Hero
-//{
-//    int hSp = 120; //высота
-//    int wSp = 80; //ширина спрайта
-//    int hSpCol = 40; //высота спрайта для столкновений
-//    int wSpCol = 60; //ширина спрайта для столкновений
-//    int firstNumCell[2][2]; //каждую итерацию герой обновляет список ячеек где находится. //11 12
-//    int SecondNumCell[2][2];                                                              //21 22 нумерация ячеек
-//    sf::Texture texture;
-//    sf::Sprite sprite;
-//    Hero()
-//    {
-//        if (!texture.loadFromFile("hero.png")) cout << "Ошибка загрузки текстуры героя\n";
-//        sprite.setTexture(texture);
-//        sprite.setOrigin(0, 120);
-//    }
-//};
-//
-////хотя бы одна точка второго спрайта в первом , то false
-//bool intersection(sf::Vector2i spr1a, sf::Vector2i spr1b, sf::Vector2i spr2a, sf::Vector2i spr2b)
-//{
-//    if (spr2a.x > spr1a.x && spr2a.x < spr1b.x && spr2a.y > spr1a.y && spr2a.y < spr1b.y ||
-//        spr2b.x > spr1a.x && spr2b.x < spr1b.x && spr2b.y > spr1a.y && spr2b.y < spr1b.y ||
-//        spr2b.x > spr1a.x && spr2b.x < spr1b.x && spr2b.y > spr1a.y && spr2b.y < spr1b.y ||
-//        spr2a.x > spr1a.x && spr2a.x < spr1b.x && spr2b.y > spr1a.y && spr2b.y < spr1b.y ||
-//        spr2b.x > spr1a.x && spr2b.x < spr1b.x && spr2a.y > spr1a.y && spr2a.y < spr1b.y
-//        /*spr2b.x - spr2a.x > spr1a.x && spr2b.x - spr2a.x < spr1b.x && spr2a.y > spr1a.y && spr2a.y < spr1b.y ||
-//        spr2a.x > spr1a.x && spr2a.x < spr1b.x && spr2b.y - spr2a.y > spr1a.y && spr2b.y - spr2a.y < spr1b.y*/) return false;
-//    return true;
-//}
-//
-//int main()
-//{
-//    setlocale(LC_ALL, "Russian");
-    
-//    bool wayClear = true; //проверяем нет ли на пути камня
-//
-//    sf::RenderWindow window(sf::VideoMode(wWidth , wHeight), "Win");
-//
-//    
-//
-//    Hero hero;
-//    hero.sprite.setPosition(0,600);
-//
-//    Grass g[heightMap][widthMap];
-//    Rock r[heightMap][widthMap];
-//
-//    //расставляю камни
-//    //тут я загружаю текстуру в каждый нужный камень и во всю траву. немного экономлю память.
-//
-//    /*for (int i = 0; i < heightMap; i++)
-//    {
-//        for (int j = 0; j < widthMap; j++)
-//        {
-//            if (!g[i][j].texture.loadFromFile("grass.png")) cout << "Ошибка загрузки текстуры травы\n";
-//            g[i][j].sprite.setTexture(g[i][j].texture);
-//            g[i][j].sprite.setPosition(i * 100, j * 100);
-//            if (j == 0 && i == 1 ||
-//                j == 1 && i == 5 ||
-//                j == 2 && i == 3 ||
-//                j == 3 && i == 2 ||
-//                j == 3 && i == 5 ||
-//                j == 4 && i == 6 ||
-//                j == 5 && i == 3
-//               )
-//            {
-//                
-//                if (!r[i][j].texture.loadFromFile("rock.png")) cout << "Ошибка загрузки текстуры камня\n";
-//                r[i][j].sprite.setTexture(r[i][j].texture);
-//                r[i][j].sprite.setPosition(i * 100, j * 100);
-//                r[i][j].origin.x = i * 100;
-//                r[i][j].origin.y = j * 100;
-//            }
-//        }
-//    }*/
-//
-//
-//    if (!r[3][3].texture.loadFromFile("rock.png")) cout << "Ошибка загрузки текстуры камня\n";
-//    r[3][3].sprite.setTexture(r[3][3].texture);
-//    r[3][3].sprite.setPosition(3 * 100, 3 * 100);
-//    r[3][3].origin.x = 3 * 100;
-//    r[3][3].origin.y = 3 * 100;
-//    cout << r[3][3].origin.x + 10 << " " << r[3][3].origin.y + 40 << endl;
-//    cout << r[3][3].origin.x + 90 << " " << r[3][3].origin.y + 100 << endl;
-//    Sleep(2000);
-//    
-//    while (window.isOpen())
-//    {
-//        sf::Vector2f heroOrigin = hero.sprite.getPosition();
-//        sf::Event event;
-//
-//        ////************
-//        sf::RectangleShape rectangle(sf::Vector2f(60.f, 40.f));
-//        rectangle.setPosition(heroOrigin.x+10 , heroOrigin.y-40);
-//        rectangle.setFillColor(sf::Color::Green);
-//        ////***********
-//
-//        //*************
-//        //for(int i = 0;i<)
-//        //*************
-//
 //        //********************************************************
 //        sf::RectangleShape line[widthMap + heightMap];
 //        for (int i = 1; i < int(wWidth / 150) + 1; i++)
@@ -178,106 +49,59 @@
 //
 //            cout << hero.firstNumCell[0][0] << " " << hero.SecondNumCell[0][0] << " " << hero.firstNumCell[0][1] << " " << hero.SecondNumCell[0][1] << endl;
 //            cout << hero.firstNumCell[1][0] << " " << hero.SecondNumCell[1][0] << " " << hero.firstNumCell[1][1] << " " << hero.SecondNumCell[1][1] << endl << endl;
-//            //Sleep(100);
-//        }
-//        while (window.pollEvent(event))
-//        {
-//            if (event.type == sf::Event::Closed)
-//                window.close();
-//            
-//        }
-//        
-//
 //        //приняли положение джойстика
 //        int dx = sf::Joystick::getAxisPosition(0, sf::Joystick::X);
 //        int dy = sf::Joystick::getAxisPosition(0, sf::Joystick::Y);
-//        
-//        //////////////////////////////////////////////////////////
-//        //проверка столкнвений. ПЕРЕПИСАТЬ!
-//        for (int i = 0; i < heightMap; i++)
-//        {
-//            for (int j = 0; j < widthMap; j++)
-//            {
-//                wayClear = intersection(sf::Vector2i(heroOrigin.x+10,heroOrigin.y-40),
-//                                        sf::Vector2i(heroOrigin.x + 70, heroOrigin.y),
-//                                        sf::Vector2i(r[i][j].origin.x+10, r[i][j].origin.y+40),
-//                                        sf::Vector2i(r[i][j].origin.x+90, r[i][j].origin.y+100));//координаты спрайта героя , затем камня
-//            }
-//        }
-//        //////////////////////////////////////////////////////////
-//
-//        //движение героя. ДЖОЙСТИК. проверка на стокновения
-//        /*if (wayClear)
-//        {
-//            hero.sprite.move(dx * 0.01, dy * 0.01);
-//        }*/
-//
-//        //столкнвения. ПЕРЕПИСАТЬ
-//        if (wayClear)
-//        {
-//            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A)) hero.sprite.move(-1, 0);
-//            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D)) hero.sprite.move(1, 0);
-//            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W)) hero.sprite.move(0, -1);
-//            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S)) hero.sprite.move(0, 1);
-//        }
-//
-//
-//
-//        window.clear();
-//        for (int i = 0; i < 6; i++)
-//        {
-//            for (int j = 0; j < 8; j++)
-//            {
-//                window.draw(g[i][j].sprite);
-//                window.draw(r[i][j].sprite);
-//            }
-//        }
-//        window.draw(hero.sprite);
-//        for (int i = 0; i < widthMap + heightMap; i++)
-//            window.draw(line[i]);
-//        window.draw(rectangle);
-//        window.display();
-//    }
-//    return 0;
-//}
-
-//не работает!
 
 const int wHeight = 600, wWidth = 800; //высота, ширина окна
-const int heightMap = 6, widthMap = 8; //колическто крадратов 100х100 травы на карте
+const int heightMap = 13, widthMap = 18; //колическто крадратов 100х100 травы на карте
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
 	sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "Win");
-	ground *gr[widthMap][heightMap];
-	bool way_clear = true;
-	bool flag = 1 , flag_y = 1;
+	window.setFramerateLimit(60);
+	sf::View view(sf::Vector2f(400.f, 300.f), sf::Vector2f(800.f, 600.f));
+	//window.setVerticalSyncEnabled(true);//ввертикальная синхронизация
+	ground* gr[widthMap][heightMap];
+	bool flag_x = 1 , flag_y = 1;
+	const int speed = 5;
 
+	ground* rocks[widthMap][heightMap];
 
 	mainHero hero((char*)"hero.png");
 
 	//расстановка камней и травы	
-	for (int i = 0; i < widthMap; i++)
+	for (int x = 0; x < widthMap; x++)//x
 	{
-		for (int j = 0; j < heightMap; j++)
+		for (int y = 0; y < heightMap; y++)//y
 		{
-			if (j == 0 && i == 1 ||
-				j == 1 && i == 5 ||
-				j == 2 && i == 3 ||
-				j == 3 && i == 2 ||
-				j == 3 && i == 5 ||
-				j == 4 && i == 6 ||
-				j == 5 && i == 3
-				) gr[i][j] = new rock();
+			if (y == 2 && (x >= 0 && x < 4) ||
+				x == 3 && (y >= 2 && y < 6) ||
+				y == 6 && (x >= 3 && x < 6) ||
+				x == 6 && (y >= 6 && y < 10) ||
+				y == 10 && (x >= 6 && x < 16) ||
+				x == 16 && (y >= 2 && y <= 10) ||
+				y == 2 && (x >= 16 && x < 18))
+				gr[x][y] = new road();
+			else if ((x >= 7 && x < 9) && (y >= 0 && y < 5) ||
+				y == 5 && (x >= 7 && x < 10) ||
+				y == 6 && (x >= 8 && x < 11) ||
+				y == 7 && (x >= 9 && x < 13) ||
+				y == 8 && (x >= 10 && x < 13) ||
+				(y >= 9 && y < 11) && (x >= 11 && x < 13) ||
+				(y >= 11 && y < 13) && (x >= 11 && x < 13))
+				gr[x][y] = new water();
 			else
-				gr[i][j] = new grass();
-			gr[i][j]->initObj(sf::Vector2i(100 * i, 100 * j));
+				gr[x][y] = new grass();
+			gr[x][y]->initObj(sf::Vector2i(100 * x, 100 * y));
 		}
 	}
 
 	while (window.isOpen())
 	{
 		hero.upDate();
+		window.setView(view);
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
@@ -286,40 +110,45 @@ int main()
 		}
 		
 		sf::Vector2i move;
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A)) move = sf::Vector2i(-1, 0);
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D)) move += sf::Vector2i(1, 0);
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W)) move += sf::Vector2i(0, -1);
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S)) move += sf::Vector2i(0, 1);
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A)) move = sf::Vector2i(-speed, 0);
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D)) move += sf::Vector2i(speed, 0);
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W)) move += sf::Vector2i(0, -speed);
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S)) move += sf::Vector2i(0, speed);
 
-		//проверка на столкновения
+		//КОЛЛИЗИИ
 		for (int i = 0; i < widthMap; i++)
 		{
 			for (int j = 0; j < heightMap; j++)
 			{
-				if (gr[i][j]->ty == ROCK)
+				if (gr[i][j]->ty == WATER)
 				{
-					flag = intersection_(hero.origin_collision_sprite + move,
-						((rock*)gr[i][j])->origin_collision_sprite,
+					/*flag_x = isIntersection(hero.origin_collision_sprite + move,
+						((water*)gr[i][j])->origin_collision_sprite,
 						hero.size_of_collision_sprite,
-						((rock*)gr[i][j])->size_of_collision_sprite);
-					if (flag) goto outFor; //если обнаружили стокновение , то сразу выходим из цикла
+						((water*)gr[i][j])->size_of_collision_sprite);*/
+
+					flag_x = isIntersectionOneCoordinatX(hero.origin_collision_sprite + move,
+						((water*)gr[i][j])->origin_collision_sprite,
+						hero.size_of_collision_sprite,
+						((water*)gr[i][j])->size_of_collision_sprite);
+
+					flag_y = isIntersectionOneCoordinatY(hero.origin_collision_sprite + move,
+						((water*)gr[i][j])->origin_collision_sprite,
+						hero.size_of_collision_sprite,
+						((water*)gr[i][j])->size_of_collision_sprite);
+					if (flag_x || flag_y) goto outFor; //если обнаружили стокновение , то сразу выходим из цикла
 				}
 			}
-		}
-
-
-		
+		}	
 	outFor://выходим вот сюда
-		//std::cout << flag << std::endl;
-		if(!flag) hero.sprite.move(move.x, move.y);
 
-
-
-
+		//зависимость от размера спрайта песонажа
+		if(!flag_x && (hero.origin_sprite.x+move.x >= 0 && hero.origin_sprite.x+move.x <= 1720)) hero.sprite.move(move.x, 0);
+		if(!flag_y && (hero.origin_sprite.y+move.y >= 0 && hero.origin_sprite.y+move.y <= 1180)) hero.sprite.move(0, move.y);
+		
 		//блок отрисовки
 		window.clear();
-
-		for (int i = 0; i <widthMap; i++)
+		for (int i = 0; i < widthMap; i++)
 		{
 			for (int j = 0; j < heightMap; j++)
 			{
@@ -327,8 +156,29 @@ int main()
 				//window.draw(gr[i][j]->sprite_of_collition_sprite);
 			}
 		}
+		//перемещение камеры
+		if (hero.origin_sprite.x > 360 && hero.origin_sprite.x <= 1360)
+			view.setCenter(hero.origin_sprite.x+40, view.getCenter().y);
+		if (hero.origin_sprite.y > 240 && hero.origin_sprite.y < 940)
+			view.setCenter(view.getCenter().x, hero.origin_sprite.y+60);
+
+		for (int i = 0; i < widthMap; i++)
+			{
+				for (int j = 0; j < heightMap; j++)
+				{
+					if (j == 0 && i == 1 ||
+						j == 1 && i == 5 ||
+						j == 2 && i == 3 ||
+						j == 3 && i == 2 ||
+						j == 3 && i == 5 ||
+						j == 4 && i == 6 ||
+						j == 5 && i == 3
+						)
+						window.draw(rocks[i][j]->sprite);
+				}
+			}
 		window.draw(hero.sprite);
-		
+
 		window.display();
 	}
 
--- objs.cpp
+++ objs.cpp
@@ -1,15 +1,23 @@
 #include "objs.h"
 
 
-bool intersection_(sf::Vector2i origin1, sf::Vector2i origin2, sf::Vector2i size1, sf::Vector2i size2)
+bool isIntersection(sf::Vector2i origin1, sf::Vector2i origin2, sf::Vector2i size1, sf::Vector2i size2)
 {
 	if (abs(origin1.x - origin2.x) > (size1.x * 0.5 + size2.x * 0.5)) return false;
 	if (abs(origin1.y - origin2.y) > (size1.y * 0.5 + size2.y * 0.5)) return false;
 	return true;
 }
 
-bool intersection_one_coordinate(int origin1, int origin2, int size1, int size2)
+bool isIntersectionOneCoordinatX(sf::Vector2i origin1, sf::Vector2i origin2, sf::Vector2i size1, sf::Vector2i size2)
 {
-	if (abs(origin1 - origin2) > (size1 * 0.5 + size2 * 0.5)) return false;
+	if (abs(origin1.y - origin2.y) >= (size1.y * 0.5 + size2.y * 0.5)) return false;
+	if (abs(origin1.x - origin2.x) > (size1.x * 0.5 + size2.x * 0.5)) return false;
+	return true;
+}
+
+bool isIntersectionOneCoordinatY(sf::Vector2i origin1, sf::Vector2i origin2, sf::Vector2i size1, sf::Vector2i size2)
+{
+	if (abs(origin1.x - origin2.x) >= (size1.x * 0.5 + size2.x * 0.5)) return false;
+	if (abs(origin1.y - origin2.y) > (size1.y * 0.5 + size2.y * 0.5)) return false;
 	return true;
 }
